2971e31a7fb934dce2fa6d6ff6268d7a8d00adc2
Zeshan Javaid <javaid.a.zeshan@gmail.com>
2025-12-18 22:28:27 -0600

    correcting previous weird commits

--- computation/algorithms/heat_addition.h
+++ computation/algorithms/heat_addition.h
@@ -6,22 +6,12 @@
 class HeatAddition {
     private:
         bool isOtto;
-<<<<<<< HEAD
-        double heating_value, work;
-=======
-        bool isClosedSystem; 
-        double heating_value;
-        double work_done;  
->>>>>>> parent of e8b4a44 (Merge branch 'main' of https://github.com/ZeshanAJavaid/Thermodynamic-Cycle-Simulator)
+        double heating_value, work_done;
+        bool isClosedSystem;
 
     public:
         HeatAddition(double, bool, bool); 
         void compute(ThermodynamicCycle&, const AirProperties&);
         double get_heating_value();
         double get_work();
-<<<<<<< HEAD
-
 };
-=======
-};
->>>>>>> parent of e8b4a44 (Merge branch 'main' of https://github.com/ZeshanAJavaid/Thermodynamic-Cycle-Simulator)
