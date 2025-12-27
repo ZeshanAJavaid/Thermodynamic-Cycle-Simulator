40afa6e16a0885a426ae8d41995da99a9ac444d1
ZeshanAJavaid <javaid.a.zeshan@gmail.com>
2025-12-27 16:23:47 -0600

    Thermodynamic Simulator v1.2.2 -> Removed the build directory

--- build/CMakeFiles/sim.dir/sim_autogen/EWIEGA46WW/qrc_images.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/sim.dir/sim_autogen/EWIEGA46WW/qrc_images.cpp.o: \
- /mnt/d/Personal-Projects/Professional-Projects/Thermodynamic-Cycle-Simulator/build/sim_autogen/EWIEGA46WW/qrc_images.cpp \
- /usr/include/stdc-predef.h
